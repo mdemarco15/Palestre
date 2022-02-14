5ba0c97ec3a04dcef6eb6a3e30dfdac77d4afc83
Notuaith <notuaith@outlook.it>
2022-02-14 16:08:04 +0100

    Pagina recupero password

--- lib/screens/recuperaPwPage/recupera_password_screen.dart
+++ lib/screens/recuperaPwPage/recupera_password_screen.dart
@@ -1,24 +1,129 @@
+import 'dart:math';
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:palestre/screens/widget/blackButton.dart';
+import 'package:palestre/screens/widget/blueButton.dart';
 
 class RecuperaPw extends StatelessWidget {
   const RecuperaPw({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: <Widget>[
-        Image.asset("assets/images/sfo.png"),
-      ],
+    return Material(
+      type: MaterialType.transparency,
+      child: new Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage("assets/images/sfo.png"), fit: BoxFit.cover),
+        ),
+        child: Column(
+          children: [
+            SizedBox(height: 380),
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 19.1),
+                  child: Text(
+                    "Recupera Password",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 34,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 10),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(width: 20),
+                Expanded(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      new Container(margin: const EdgeInsets.all(5)),
+                      Text(
+                        "Inserisci l'email usata per la registrazione per ricevere il link di recupero.",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 15,
+                            fontWeight: FontWeight.normal),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 45),
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 20),
+                  child: Text(
+                    "Email",
+                    style: TextStyle(
+                        color: Color.fromARGB(255, 158, 158, 158),
+                        fontSize: 17,
+                        fontWeight: FontWeight.normal),
+                  ),
+                ),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Container(
+                height: 50,
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                      enabledBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white),
+                      ),
+                      focusedBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white),
+                      ),
+                      hintText: "Inserisci Email",
+                      hintStyle: TextStyle(color: Colors.grey, fontSize: 15)),
+                  cursorColor: Colors.white,
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 17,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 50),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                GestureDetector(
+                  onTap: () => null,
+                  child: BlueButton(
+                    testo: 'Invia',
+                  ),
+                )
+              ],
+            ),
+            SizedBox(height: 21),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                GestureDetector(
+                  onTap: () => null,
+                  child: BlackButton(
+                    testo: 'Indietro',
+                  ),
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
     );
-//     return Container(
-//       width: MediaQuery.of(context).size.width,
-//       height: MediaQuery.of(context).size.height,
-//       decoration: const BoxDecoration(
-//         image: DecorationImage(
-//         image: ExactAssetImage("assets/images/sfo.png"),
-//         fit: BoxFit.fitHeight,
-//         )
-//       ),
-//     );
   }
 }
--- lib/screens/welcomePage/welcome_page_screen.dart
+++ lib/screens/welcomePage/welcome_page_screen.dart
@@ -11,72 +11,71 @@ class WelecomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        resizeToAvoidBottomInset: false,
+      resizeToAvoidBottomInset: false,
       body: new SingleChildScrollView(
         child: new Column(
           children: <Widget>[
             Container(
-                width: MediaQuery.of(context).size.width,
-                height: MediaQuery.of(context).size.height,
-                decoration: const BoxDecoration(
-                    image: DecorationImage(
-                      image: AssetImage("assets/images/sfo.png"), fit: BoxFit.cover)
-                   
-                ),
-                child: Column(
-                  //  mainAxisAlignment: MainAxisAlignment.center ,
-                  children: [
-                    SizedBox(height: 380),
-                    Row(
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.only(left: 13.0),
-                          child: Text(
-                            "Sign in",
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 34,
-                                fontWeight: FontWeight.bold),
-                          ),
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
+              decoration: const BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage("assets/images/sfo.png"),
+                      fit: BoxFit.cover)),
+              child: Column(
+                //  mainAxisAlignment: MainAxisAlignment.center ,
+                children: [
+                  SizedBox(height: 380),
+                  Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(left: 13.0),
+                        child: Text(
+                          "Sign in",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 34,
+                              fontWeight: FontWeight.bold),
                         ),
-                      ],
-                    ),
-                    SizedBox(height: 10),
-                    Row(
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.only(left: 13.0),
-                          child: Text(
-                            "Benvenuto nella nostra Palestra",
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 17,
-                                fontWeight: FontWeight.normal),
-                          ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 10),
+                  Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(left: 13.0),
+                        child: Text(
+                          "Benvenuto nella nostra Palestra",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 17,
+                              fontWeight: FontWeight.normal),
                         ),
-                      ],
-                    ),
-                    SizedBox(height: 20),
-                    Row(
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.only(left: 13.0),
-                          child: Text(
-                            "Email",
-                            style: TextStyle(
-                                color: Color.fromARGB(255, 158, 158, 158),
-                                fontSize: 17,
-                                fontWeight: FontWeight.normal),
-                          ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 20),
+                  Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(left: 13.0),
+                        child: Text(
+                          "Email",
+                          style: TextStyle(
+                              color: Color.fromARGB(255, 158, 158, 158),
+                              fontSize: 17,
+                              fontWeight: FontWeight.normal),
                         ),
-                      ],
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 13),
-                      child: Container(
-                        height: 35,
-                        child: TextFormField(
-                          decoration: const InputDecoration(
+                      ),
+                    ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 13),
+                    child: Container(
+                      height: 35,
+                      child: TextFormField(
+                        decoration: const InputDecoration(
                             enabledBorder: UnderlineInputBorder(
                               borderSide: BorderSide(color: Colors.white),
                             ),
@@ -84,40 +83,39 @@ class WelecomePage extends StatelessWidget {
                               borderSide: BorderSide(color: Colors.white),
                             ),
                             hintText: "Inserisci Email",
-                            hintStyle: TextStyle(color: Colors.grey, fontSize: 15)
-                          ),
-                          cursorColor: Colors.white,
-                          style: const TextStyle(
-                            color: Colors.white,
-                            fontSize: 17,
-                          ),
+                            hintStyle:
+                                TextStyle(color: Colors.grey, fontSize: 15)),
+                        cursorColor: Colors.white,
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 17,
                         ),
                       ),
                     ),
-                    SizedBox(height: 30),
-                    Row(
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.only(left: 13.0),
-                          child: Text(
-                            "Password",
-                            style: TextStyle(
-                                color: Color.fromARGB(255, 158, 158, 158),
-                                fontSize: 17,
-                                fontWeight: FontWeight.normal),
-                          ),
+                  ),
+                  SizedBox(height: 30),
+                  Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(left: 13.0),
+                        child: Text(
+                          "Password",
+                          style: TextStyle(
+                              color: Color.fromARGB(255, 158, 158, 158),
+                              fontSize: 17,
+                              fontWeight: FontWeight.normal),
                         ),
-                      ],
-                    ), 
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 13),
-                      child: Container(
-                        height: 35,
-                        child: TextFormField(
-                          obscureText: true,
-                          autofocus: false,
-                          decoration: const InputDecoration(
-                            
+                      ),
+                    ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 13),
+                    child: Container(
+                      height: 35,
+                      child: TextFormField(
+                        obscureText: true,
+                        autofocus: false,
+                        decoration: const InputDecoration(
                             enabledBorder: UnderlineInputBorder(
                               borderSide: BorderSide(color: Colors.white),
                             ),
@@ -125,54 +123,65 @@ class WelecomePage extends StatelessWidget {
                               borderSide: BorderSide(color: Colors.white),
                             ),
                             hintText: "Inserisci Password",
-                             hintStyle: TextStyle(color: Colors.grey, fontSize: 15)
-                          ),
-                          cursorColor: Colors.white,
-                          style: const TextStyle(
-                            color: Colors.white,
-                            fontSize: 17,
-                          ),
+                            hintStyle:
+                                TextStyle(color: Colors.grey, fontSize: 15)),
+                        cursorColor: Colors.white,
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 17,
                         ),
                       ),
                     ),
-                    SizedBox(height: 17),
-                    Padding(
-                      padding: const EdgeInsets.only(right:13.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
-                            GestureDetector(
-                              onTap: () => Navigator.push(
-                                 context,
-                                  MaterialPageRoute(builder: (context) => const RecuperaPw())),
-                              child: Text("Password dimenticata?",
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 13,
-                              ),),
-                            )
-                      ],),
-                    ),
-                    SizedBox(height: 25),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
+                  ),
+                  SizedBox(height: 17),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 13.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
                       children: [
                         GestureDetector(
-                          onTap: () => null,
-                          child: BlueButton(testo: 'Accedi',))
+                          onTap: () => Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => const RecuperaPw())),
+                          child: Text(
+                            "Password dimenticata?",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 13,
+                            ),
+                          ),
+                        )
                       ],
                     ),
-                    SizedBox(height: 21),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        GestureDetector(
-                          onTap: () => null,
-                          child: BlackButton(testo: 'Iscriviti',))
-                      ],
-                    ),
-                  ],
-                )),
+                  ),
+                  SizedBox(height: 25),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      GestureDetector(
+                        onTap: () => null,
+                        child: BlueButton(
+                          testo: 'Accedi',
+                        ),
+                      )
+                    ],
+                  ),
+                  SizedBox(height: 21),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      GestureDetector(
+                        onTap: () => null,
+                        child: BlackButton(
+                          testo: 'Iscriviti',
+                        ),
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
